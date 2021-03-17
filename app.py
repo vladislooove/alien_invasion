7101ab2dff6a4a1b1e886383d07dda476f0362e3
Vlad Prozapas <fadetoblack1@yandex.ru>
2021-03-17 21:04:28 +0200

    Moved game config to separate class

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_app():
     # Inits game and creates screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    settings = Settings()
+    screen = pygame.display.set_mode((settings.screen_width, screen_height))
     pygame.display.set_caption("Alien Invasion") 
-
+    
     # Running main game loop
     while True:
         # Catching keyboard events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            
+
+            # Set screen background            
+            screen.fill(settings.bg_color)
+
             # Display the last rendered screen
             pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    def __init__(self):
+        # Init game settings
+        # Screen params
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
